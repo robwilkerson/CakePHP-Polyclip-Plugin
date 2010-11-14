0b3c367f476b0ffc09f2017c546b9c03bb6d5b06
Rob Wilkerson <rob@robwilkerson.org>
2010-11-14 16:11:55 -0500

    Removing a few other odds and ends that would allow the plugin to have too much control. We want to love most of this stuff to the application.

--- webroot/js/admin.js
+++
@@ -1,6 +0,0 @@
-/**
- * Scripts used to support admin actions on an attachment.
- */
-$(document).ready( function() {
-	$('.input.attachment a').colorbox({ href: '/polyclip/admin/attachments/edit/' });
-});
