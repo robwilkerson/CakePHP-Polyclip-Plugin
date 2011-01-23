a665df66489492044d40190295a16b677a74c324
Rob Wilkerson <rob@robwilkerson.org>
2011-01-22 22:10:41 -0500

    Creating the plugin's webroot directory.
    The webroot directory doesn't exist by default, so the script creates
    it before doing anything else.

--- config/install/mkassets.sh
+++ config/install/mkassets.sh
@@ -3,6 +3,10 @@
 basedir=../../webroot
 letters="a b c d e f g h i j k l m n o p q r s t u v q r s t u v w x y z"
 
+if [ ! -d $basedir ]; then
+  mkdir $basedir
+fi
+
 cd $basedir
 mkdir -p assets/_tmp
 cd assets 
