03747b8b03515257efecc18c06c6d5d31d1525b9
Rob Wilkerson <rob@robwilkerson.org>
2011-01-31 21:19:39 -0500

    Making the specified directory if it doesn't exist.

--- config/install/install.sh
+++ config/install/install.sh
@@ -5,11 +5,6 @@ if [ $# -eq 0 ]; then
      exit 1
 fi
 
-if [ ! -d $1 ]; then
-     echo "The specified directory ($1) does not exist"
-     exit 1
-fi
-
 basedir=$1
 letters="a b c d e f g h i j k l m n o p q r s t u v q r s t u v w x y z"
 
